b9673f01d5a87abed95676232d03370f46590a9c
Denis Belousov <109822229+denis-bw@users.noreply.github.com>
2023-09-18 13:02:07 +0300

    fixed contact update

--- models/contacts.js
+++ models/contacts.js
@@ -31,7 +31,8 @@ const updateContact = async (id, body) => {
   const contacts = await listContacts();
   const index = contacts.findIndex((contact) => contact.id === id);
   if (index === -1) return null;
-  contacts[index] = { id, ...body };
+  
+  contacts[index] = { ...contacts[index], ...body };
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return contacts[index];
 };
--- routes/api/contacts-routes.js
+++ routes/api/contacts-routes.js
@@ -7,13 +7,10 @@ const router = express.Router()
 
 const addSchema = Joi.object({
   name: Joi.string()
-    .required()
     .messages({ "any.required": "Missing required name field" }),
   email: Joi.string()
-    .required()
     .messages({ "any.required": "Missing required email field" }),
   phone: Joi.string()
-    .required()
     .messages({ "any.required": "Missing required phone field" }),
 });
 
