17eff4a970e1cc6a1f33016db9465ced37038319
Denis Belousov <109822229+denis-bw@users.noreply.github.com>
2023-09-26 00:01:03 +0300

    protection of contacts by owner

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -4,7 +4,11 @@ import { httpError } from "../helpers/index.js";
 
 
 const listContacts = async (req, res) => {
-    const contacts = await Contact.find();
+    const {_id: owner} = req.user;
+    const {page = 1, limit = 10} = req.query;
+    const skip = (page - 1) * limit;
+    const contacts = await Contact.find({owner}, "-createdAt -updatedAt", {skip, limit})
+        .populate("owner", "name email");
     res.json(contacts);
 };
 
