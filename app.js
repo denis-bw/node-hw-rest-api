de3a1543a680469943152424b05c3382c5cbe011
Denis Belousov <109822229+denis-bw@users.noreply.github.com>
2023-09-25 18:49:09 +0300

    add-auth

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import logger from 'morgan';
 import cors from 'cors';
 
 import contactRouter from './routes/api/contacts-routes.js';
+import authRouter from "./routes/api/auth-router.js";
 
 const app = express()
 
@@ -12,6 +13,7 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
+app.use("/api/auth", authRouter);
 app.use('/api/contacts', contactRouter)
 
 app.use((req, res) => {
